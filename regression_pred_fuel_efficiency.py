14585cd42ba58a36a5879ee7ea443b424f433e3b
MeiJamke <meijamke@gmail.com>
2019-05-01 23:57:49 +0800

    download dataset + preprocess dataset + split dataset

--- regression_pred_fuel_efficiency.py
+++ regression_pred_fuel_efficiency.py
@@ -7,7 +7,7 @@
     回归问题：从连续值中预测一个值
 
     # Use seaborn for pairplot
-    !pip install -q seaborn
+    pip install -q seaborn
 """
 
 from __future__ import absolute_import, division, print_function
@@ -22,3 +22,40 @@ import tensorflow as tf
 import keras
 from keras import layers
 
+"""
+    下载数据集
+    使用pandas导入
+"""
+dataset_path = keras.utils.get_file('auto-mpg.data',
+                                    'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data')
+print(dataset_path)
+
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                          na_values='?', comment='\t',
+                          sep='', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+print(dataset.tail())
+
+"""
+    预处理数据
+"""
+dataset.isna().sum()
+dataset = dataset.dropna()
+
+origin = dataset.pop('Origin')
+dataset['USA'] = (origin == 1) * 1.0
+dataset['Europe'] = (origin == 2) * 1.0
+dataset['Japan'] = (origin == 3) * 1.0
+
+print(dataset.tail())
+
+"""
+    切分数据集
+    训练集+测试集
+"""
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
